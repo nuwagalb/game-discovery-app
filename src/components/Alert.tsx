329e63cf106b4cf864ece180ed383becd9096366
nuwagalb <isnuwa@gmail.com>
2023-06-19 16:16:26 +0300

    edit alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,13 +1,26 @@
 //Component that accepts children
 import { ReactNode } from "react";
 interface Props {
-    children: ReactNode;
+  children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
-  )
-}
+    <div
+      className="alert alert-primary alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>My Alert</strong>
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
 
-export default Alert
+export default Alert;
